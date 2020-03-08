8764de2823fbab0a406d43a211bc75154ed04c96
ashinjohn <ashinbasiljohn@gmail.com>
2020-03-08 21:35:14 +0530

    Battery Voltage Measurement

--- BatteryCapacityChecker.ino
+++ BatteryCapacityChecker.ino
@@ -12,10 +12,25 @@
 
 */
 
+/* Probe Color and Battery
+   RED -> B1
+   YELLOW -> B2
+   GREEN -> B2
+   ORANGE -> B2
+*/
+
 // 16x 2 LCD Configuration HD44780
 #include <LiquidCrystal.h>
 const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//Battery Voltage Measurement
+int BAT_V1 = A5, BAT_V2 = A4, BAT_V3 = A3, BAT_V4 = A2;
+
+float GetBATVoltage(int Battery)
+{
+  return (analogRead(Battery) * (5.0 / 1024.0));
+}
 
 void setup() {
   // Print Code Revision to the LCD.
@@ -27,5 +42,6 @@ void setup() {
 }
 
 void loop() {
-
+  lcd.setCursor(0, 1);
+  lcd.print(GetBATVoltage(BAT_V4));
 }
