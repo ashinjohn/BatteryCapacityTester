34f01e5a37b54eb108d622e80c0ed9f00cfd2031
ashinjohn <ashinbasiljohn@gmail.com>
2020-03-08 17:44:23 +0530

    Writeup Description

---
+++ BatteryCapacityChecker.ino
@@ -0,0 +1,22 @@
+/*
+   Battery Capacity Checker
+
+   Description: Estimates the capacity of batteries by discharging them to batterylow threshold voltage.
+   Firmware fine tuned for 18650 Lithium Battery
+   4 channels supporting 30V or 3A ( Not both at a time, Heat dissipation issues)
+   IDE -> Arduino 
+   MCU -> ATmega328P Barebone Arduino
+
+   Hardware: Dummy Load Circuit, 16x 2 LCD Module 
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
