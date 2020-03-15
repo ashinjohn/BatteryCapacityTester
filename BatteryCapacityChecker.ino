de38f49f26e3a53722d8824023d4b55937b3de4c
ashinjohn <ashinbasiljohn@gmail.com>
2020-03-15 23:43:48 +0530

    Application for Single Channel

--- BatteryCapacityChecker.ino
+++ BatteryCapacityChecker.ino
@@ -14,11 +14,20 @@
 
 */
 
+// 16x 2 LCD Configuration HD44780
+#include <LiquidCrystal.h>
+const int rs = 4, en = 3, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 #include "Timer.h"
 Timer t;
 
+//Battery AH
+float AH_R = 0, AH_Y = 0, AH_G = 0, AH_O = 0;
+
 // Battery Discharge Termination Pin Mapping
 const int TER_R = 9, TER_Y = 10, TER_G = 11, TER_O = 12;
+bool Rdischarge = false, Ydischarge = false, Gdischarge = false, Odischarge = false;
 
 void START_DIS(int DISCHARGE_ENABLE_PIN) {
   digitalWrite(DISCHARGE_ENABLE_PIN, LOW );
@@ -28,17 +37,13 @@ void END_DIS(int DISCHARGE_ENABLE_PIN) {
   digitalWrite(DISCHARGE_ENABLE_PIN, HIGH);
 }
 
-void DISABLE_DISCHARGE() {
+void DISABLE_ALL_DISCHARGE() {
   Serial.println();
   Serial.println("Terminating all Discharges");
+  Rdischarge = false, Ydischarge = false, Gdischarge = false, Odischarge = false;
   END_DIS(TER_R); END_DIS(TER_Y); END_DIS(TER_O); END_DIS(TER_G);
   delay(100);
 }
-
-// 16x 2 LCD Configuration HD44780
-#include <LiquidCrystal.h>
-const int rs = 4, en = 3, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 //Battery Voltage Measurement
 volatile float BAT_VR = 0.0, BAT_VY = 0.0, BAT_VG = 0.0, BAT_VO = 0.0;
@@ -53,10 +58,10 @@ void RefreshBATVoltage()
 {
   Serial.println();
   Serial.println("Battery Voltages");
-  BAT_VR = GetBATVoltage(VR); delay(100); Serial.print("BAT Voltage R :"); Serial.println(BAT_VR);
-  BAT_VY = GetBATVoltage(VY); delay(100); Serial.print("BAT Voltage Y :"); Serial.println(BAT_VY);
-  BAT_VG = GetBATVoltage(VG); delay(100); Serial.print("BAT Voltage G :"); Serial.println(BAT_VG);
-  BAT_VO = GetBATVoltage(VO); delay(100); Serial.print("BAT Voltage O :"); Serial.println(BAT_VO);
+  BAT_VR = GetBATVoltage(VR); delay(100); //Serial.print("BAT Voltage R :"); Serial.println(BAT_VR);
+  BAT_VY = GetBATVoltage(VY); delay(100); //Serial.print("BAT Voltage Y :"); Serial.println(BAT_VY);
+  BAT_VG = GetBATVoltage(VG); delay(100); //Serial.print("BAT Voltage G :"); Serial.println(BAT_VG);
+  BAT_VO = GetBATVoltage(VO); delay(100); //Serial.print("BAT Voltage O :"); Serial.println(BAT_VO);
   Serial.println();
 }
 
@@ -64,20 +69,62 @@ void RefreshBATVoltage()
 const int PushbuttonPin = 2;
 int PushbuttonState = 0;
 
+uint32_t TIM_IN_R = 0, TIM_IN_Y = 0, TIM_IN_G = 0, TIM_IN_O = 0;
+
 void ButtonPressed() {
-  Serial.println("------------------------------------------------>HIT");
+  //Initiate discharge at begining only
+  //  if (discharge == false && AH_R == 0 && AH_Y == 0 && AH_G == 0 && AH_O == 0) {
+  RefreshBATVoltage();
+  //Recording initial millis
+  /*
+    BAT_TR = millis();
+    BAT_TY = millis();
+    BAT_TG = millis();
+  */
+
+  //Stopping all on going discharge when PB is pressed and wait till next press
+  if (Odischarge == true || Gdischarge == true || Ydischarge == true || Rdischarge == true ) {
+    DISABLE_ALL_DISCHARGE();
+    Serial.println("EMERGENCY STOP VIA PB");
+  }
+  else {
+
+    if (Odischarge == false && BAT_VO > 3.7) {
+      // AH value preserved if available
+      if ( AH_O == 0) {
+        Serial.println("STARTING FROM 0 AH");
+        TIM_IN_O = millis();
+      }
+      else
+      {
+        Serial.println("OLD AH VALUE PRESERVED");
+      }
+      Odischarge = true;
+    }
+  }
 }
 
-
 void IncrementAh() {
-  Serial.println("------------------------------------------------>HIT");
-  Serial.print("2 second tick: millis()=");
-  Serial.println(millis());
+  AH_R = AH_R + 1;
+  AH_Y = AH_Y + 1;
+  AH_G = AH_G + 1;
+  AH_O = AH_O + 1;
+
+  /*
+    Serial.print(" AHR=");
+    Serial.print(AH_R);
+    Serial.print(" AHY=");
+    Serial.print(AH_Y);
+    Serial.print(" AHG=");
+    Serial.print(AH_G);
+    Serial.print(" AHO=");
+    Serial.println(AH_O);
+  */
 }
 
 void setup() {
   //Timer for incrementing AH per second
-  int tickEvent = t.every(1000, IncrementAh);
+  //int tickEvent = t.every(1000, IncrementAh);
 
 
   // Pin Mode configuration for Pushbutton Input with Internal Pullup
@@ -98,11 +145,90 @@ void setup() {
   Serial.println("R 1.0");
   lcd.setCursor(0, 1);
   lcd.print("R 1.0");
+  DISABLE_ALL_DISCHARGE();
   delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print("Push to Start");
 }
 
 void loop() {
-  t.update();
-  //RefreshBATVoltage();
-  //DISABLE_DISCHARGE();
+  t.update();// Timer for AH calc
+  RefreshBATVoltage();
+
+  //Printing Capacity on LCD
+  lcd.clear();
+
+  Serial.print("BAT Voltage O :"); Serial.println(BAT_VO);
+  Serial.print("Odischarge"); Serial.println(Odischarge);
+  lcd.setCursor(0, 0);
+  lcd.print("O= ");
+
+
+  if ( BAT_VO >= 3.0 && Odischarge == true) {
+    AH_O = ((millis() - TIM_IN_O) * 0.001) / 3600; //in AH
+    Serial.println("Discharging Orange Battery");
+
+  }
+  else
+  {
+    Serial.println("NOT -Discharging Orange Battery");
+    Odischarge = false;
+    END_DIS(TER_O);
+  }
+  lcd.print(AH_O);
+
+  delay(500);
+  lcd.setCursor(0, 0);
+  lcd.print("O= ");
+  lcd.print(BAT_VO);
+  delay(500);
+
 }
+/*
+
+  lcd.setCursor(0, 0);
+  lcd.print(((millis() - TIM_IN_R) * 0.001) / 3600);
+  Serial.println((millis() - ts1) * 0.001);
+  }
+
+
+
+
+  lcd.print(AH_R / 3600);
+  lcd.setCursor(8, 0);
+  lcd.print("Y= ");
+  lcd.print(AH_Y / 3600);
+  lcd.setCursor(0, 1);
+  lcd.print("G= ");
+  lcd.print(AH_G / 3600);
+  lcd.setCursor(8, 1);
+  lcd.print("O= ");
+  lcd.print(AH_O / 3600);
+  }
+
+  //RefreshBATVoltage();
+  /*
+  if ( BAT_VR < 3.0 ) {
+    END_DIS(TER_R);
+    lcd.setCursor(0, 0);
+    lcd.print("BAT RED DONE");
+    Serial.println("BAT RED DONE");
+  }
+
+  if ( BAT_VG < 3.0 ) {
+    END_DIS(TER_G);
+    lcd.setCursor(0, 0);
+    lcd.print("BAT GRE DONE");
+    Serial.println("BAT GRE DONE");
+  }
+
+  if ( BAT_VY < 3.0 ) {
+    END_DIS(TER_Y);
+    lcd.setCursor(0, 0);
+    lcd.print("BAT YEL DONE");
+    Serial.println("BAT YEL DONE");
+  }
+
+  //DISABLE_DISCHARGE();
+  }
+*/
